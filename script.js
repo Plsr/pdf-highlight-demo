3eefcc6ca7848a3d154103df4767701cf152b10b
Plsr <christianpoplawski@gmail.com>
2017-05-11 09:48:49 +0200

    Basic pdf highlight demo

---
+++ script.js
@@ -0,0 +1,107 @@
+console.log("Howdy, world"); // DEBUG
+
+var url = "http://cdn.mozilla.net/pdfjs/tracemonkey.pdf"
+PDFJS.workerSrc = 'http://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+var pdfDoc = null,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 1,
+    canvas = document.getElementById('the-canvas'),
+    ctx = canvas.getContext('2d');
+
+function renderPage(num) {
+  pageRendering = true;
+  // Using promise to fetch the page
+  pdfDoc.getPage(num).then(function(page) {
+    var viewport = page.getViewport(scale);
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+
+    // Wait for rendering to finish
+    renderTask.promise.then(function() {
+      pageRendering = false;
+      highlightsForPage(num, ctx)
+      if (pageNumPending !== null) {
+        // New page rendering is pending
+        renderPage(pageNumPending);
+        pageNumPending = null;
+      }
+    });
+  });
+
+  // Update page counters
+  document.getElementById('page_num').textContent = pageNum;
+}
+
+/**
+ * If another page rendering in progress, waits until the rendering is
+ * finised. Otherwise, executes rendering immediately.
+ */
+function queueRenderPage(num) {
+  if (pageRendering) {
+    pageNumPending = num;
+  } else {
+    renderPage(num);
+  }
+}
+
+function highlightsForPage(pageNum, ctx) {
+  switch (pageNum) {
+    case 1:
+      ctx.strokeStyle = "red"
+      ctx.strokeRect(220,205,170,80);
+      ctx.strokeRect(50,330,60,20);
+      break;
+      case 2:
+        ctx.strokeStyle = "red"
+        ctx.strokeRect(320,180,230,190);
+        ctx.strokeRect(310,525,200,20);
+        break;
+    default:
+
+  }
+}
+
+/**
+ * Displays previous page.
+ */
+function onPrevPage() {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+}
+document.getElementById('prev').addEventListener('click', onPrevPage);
+
+/**
+ * Displays next page.
+ */
+function onNextPage() {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+}
+document.getElementById('next').addEventListener('click', onNextPage);
+
+/**
+ * Asynchronously downloads PDF.
+ */
+PDFJS.getDocument(url).then(function(pdfDoc_) {
+  pdfDoc = pdfDoc_;
+  document.getElementById('page_count').textContent = pdfDoc.numPages;
+
+  // Initial/first page rendering
+  renderPage(pageNum);
+});
