14ad869c898947ecd3d1e0f4d4af782d77a2d267
Plsr <christianpoplawski@gmail.com>
2017-05-11 09:54:05 +0200

    Use even more https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Howdy, world"); // DEBUG
 
 var url = "https://cdn.mozilla.net/pdfjs/tracemonkey.pdf"
-PDFJS.workerSrc = 'http://mozilla.github.io/pdf.js/build/pdf.worker.js';
+PDFJS.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
 var pdfDoc = null,
     pageNum = 1,
